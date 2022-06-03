adf1b2f434e502c36ddbddf134f622371427e631
Joseph Naberhaus <naberhausj@gmail.com>
2022-06-03 01:33:46 -0500

    feat: generate csvs

--- index.ts
+++ index.ts
@@ -17,8 +17,22 @@ function main() {
 function createContainers(numContainers: number): void {
     const uuids: string[] = [];
     for (let i = 0; i < numContainers; i++) {
-        uuids.push(uuid());
+        uuids.push(uuid().toUpperCase());
     }
 
-    console.log(uuids);
+    downloadCSV(uuids);
+}
+
+function downloadCSV(uuids: string[]): void {
+    const csvData = uuids.join('\n');
+    const blob = new Blob([csvData], {type: 'text/csv'});
+    const url = URL.createObjectURL(blob);
+
+    const downloadLink = document.createElement('a');
+    downloadLink.href = url;
+    downloadLink.download = 'containers.csv';
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    document.body.removeChild(downloadLink);
+    URL.revokeObjectURL(url);
 }
