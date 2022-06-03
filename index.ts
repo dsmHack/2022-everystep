48bed33450bc56bb6e88dd274982ce0fefda8c8e
Joseph Naberhaus <naberhausj@gmail.com>
2022-06-03 01:14:41 -0500

    feat: generate uuids

--- index.ts
+++ index.ts
@@ -1,1 +1,24 @@
-console.log('test');
+import { v4 as uuid } from 'uuid';
+
+window.onload = main;
+function main() {
+    const numberOfContainerInput = document.getElementById('numberOfContainers') as HTMLInputElement;
+    const createContainersButton = document.getElementById('createContainers') as HTMLInputElement;
+
+    numberOfContainerInput.oninput = () => {
+        createContainersButton.disabled = !numberOfContainerInput.value
+    }
+
+    createContainersButton.onclick = () => {
+        createContainers(parseFloat(numberOfContainerInput.value));
+    }
+}
+
+function createContainers(numContainers: number): void {
+    const uuids: string[] = [];
+    for (let i = 0; i < numContainers; i++) {
+        uuids.push(uuid());
+    }
+
+    console.log(uuids);
+}
